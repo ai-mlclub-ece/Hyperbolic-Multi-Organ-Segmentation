23e94d21de93cbd0eddc069fc75a2813ea8c3c6f
Manasa <yelamarthymanasa@gmail.com>
2025-03-24 19:32:36 +0530

    fix error

--- configs/__init__.py
+++ configs/__init__.py
@@ -34,5 +34,5 @@ class allConfig(Config):
     def get_config_filename(self):
         filename = f"{self.train_config['model']}_{self.train_config['loss']}-v{self.version}"
         if os.path.exists(self.all_configs_dir + f"{filename}.json"):
-            filename = f"{self.train_config["model"]}_{self.train_config["loss"]}-v{self.version + 1}"
-        return filename
+            filename = f"{self.train_config['model']}_{self.train_config['loss']}-v{self.version + 1}"
+        return self.all_configs_dir + filename
